f22ae9d0726b48338fded86d9cc8ef9f0eb5ee6a
mikeh <mikethomashaber@gmail.com>
2015-08-02 13:27:45 +0100

    First commit of mind map

---
+++ Dyslixia.mm
@@ -0,0 +1,114 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Dyslixia" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1438514020906"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node TEXT="Material Reasoning" POSITION="right" ID="ID_1594393908" CREATED="1438514021711" MODIFIED="1438514033818">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="Interconnected Reasoning" POSITION="right" ID="ID_515354430" CREATED="1438514035030" MODIFIED="1438514056624">
+<edge COLOR="#0000ff"/>
+<node TEXT="Similarities" ID="ID_218722766" CREATED="1438514091815" MODIFIED="1438514095563">
+<node TEXT="Objects" ID="ID_1386825534" CREATED="1438514096055" MODIFIED="1438514098331"/>
+<node TEXT="Structures" ID="ID_1134716081" CREATED="1438514098808" MODIFIED="1438514103995"/>
+<node TEXT="Physical Patterns" ID="ID_1677230642" CREATED="1438514104344" MODIFIED="1438514107891"/>
+<node TEXT="Works by" ID="ID_1838716950" CREATED="1438514120429" MODIFIED="1438514124920">
+<node TEXT="Designers" ID="ID_1630720347" CREATED="1438514125197" MODIFIED="1438514127896"/>
+<node TEXT="Architects" ID="ID_829044387" CREATED="1438514128181" MODIFIED="1438514131272"/>
+<node TEXT="artists" ID="ID_868233682" CREATED="1438514131525" MODIFIED="1438514136040"/>
+</node>
+<node TEXT="words and verbal concepts" ID="ID_221465913" CREATED="1438514165181" MODIFIED="1438514193941">
+<node TEXT="analogies" ID="ID_1698037006" CREATED="1438514173130" MODIFIED="1438514206114"/>
+<node TEXT="metahors" ID="ID_626725184" CREATED="1438514206631" MODIFIED="1438514215079"/>
+<node TEXT="paradoxes" ID="ID_1798531004" CREATED="1438514215476" MODIFIED="1438514219311"/>
+<node TEXT="alternete word or paradox meanings" ID="ID_639664284" CREATED="1438514219708" MODIFIED="1438514240029"/>
+<node TEXT="Verbal and comceptual connections" ID="ID_691221614" CREATED="1438514296154" MODIFIED="1438514314812">
+<node TEXT="Broad field of possible meanings" ID="ID_422416143" CREATED="1438514315281" MODIFIED="1438514326372"/>
+<node TEXT="unusual or creative answers" ID="ID_1932161820" CREATED="1438514336879" MODIFIED="1438514343035"/>
+</node>
+</node>
+<node TEXT="Conceptual linking" ID="ID_1190827272" CREATED="1438514378145" MODIFIED="1438514383036"/>
+</node>
+<node TEXT="Togetherness" ID="ID_248757749" CREATED="1438514549768" MODIFIED="1438514558740">
+<node TEXT="Correlation" ID="ID_37826890" CREATED="1438514559600" MODIFIED="1438514570033"/>
+<node TEXT="Cause and Effect" ID="ID_889749174" CREATED="1438514570582" MODIFIED="1438514575105"/>
+<node TEXT="Pattern Detection" ID="ID_1185971626" CREATED="1438514586036" MODIFIED="1438514590024"/>
+<node TEXT="Things are as they are because of their relationship with everything else" ID="ID_1358127325" CREATED="1438514617004" MODIFIED="1438514641545"/>
+</node>
+<node TEXT="Connections between perspectives" ID="ID_267465920" CREATED="1438514711202" MODIFIED="1438514723567">
+<node TEXT="Prefer interdiciplinary approaches" ID="ID_1143382176" CREATED="1438516876598" MODIFIED="1438516885353"/>
+<node TEXT="Reject categorizing knowldge into diciplines" ID="ID_671161412" CREATED="1438516886365" MODIFIED="1438516914645"/>
+<node TEXT="Leads to multiple specialists" ID="ID_203358553" CREATED="1438516933231" MODIFIED="1438516940058"/>
+<node TEXT="&apos;interesting questions require new approaches" ID="ID_679316955" CREATED="1438516966534" MODIFIED="1438516984699"/>
+</node>
+<node TEXT="Global Thinking" ID="ID_1073373115" CREATED="1438517212158" MODIFIED="1438517220117">
+<node TEXT="Combine different type o sinformation into global view." ID="ID_1609080689" CREATED="1438517220538" MODIFIED="1438517238390"/>
+<node TEXT="Due to fundamental difference in way dyslexic brains are organised" ID="ID_531509478" CREATED="1438517557970" MODIFIED="1438517587088"/>
+<node TEXT="Gist" ID="ID_1963992366" CREATED="1438517596701" MODIFIED="1438517599927">
+<node TEXT="contextual" ID="ID_378959685" CREATED="1438517681879" MODIFIED="1438517686625"/>
+<node TEXT="Forest rather then trees" ID="ID_348585156" CREATED="1438517692659" MODIFIED="1438517698095"/>
+<node TEXT="fitness of analagies or metaphors" ID="ID_655809449" CREATED="1438517888366" MODIFIED="1438517902787">
+<node TEXT="which" ID="ID_128721283" CREATED="1438517903120" MODIFIED="1438517944392">
+<node TEXT="Perspective" ID="ID_1153892404" CREATED="1438517944506" MODIFIED="1438517947928"/>
+<node TEXT="Viewpoint" ID="ID_174108985" CREATED="1438517948180" MODIFIED="1438517950897"/>
+<node TEXT="Approach" ID="ID_1043050184" CREATED="1438517951604" MODIFIED="1438517969853"/>
+<node TEXT="to use" ID="ID_387347311" CREATED="1438517970090" MODIFIED="1438517972173"/>
+</node>
+</node>
+</node>
+<node TEXT="Search for gist and context" ID="ID_58277326" CREATED="1438518133066" MODIFIED="1438518144882">
+<node TEXT="look for deeper layers of meaning" ID="ID_1802766181" CREATED="1438518145118" MODIFIED="1438518153482"/>
+<node TEXT="Question ideas taken for granted" ID="ID_225227851" CREATED="1438518165417" MODIFIED="1438518181431"/>
+<node TEXT="Find secrets hidden in plain sight" ID="ID_1502429329" CREATED="1438518187086" MODIFIED="1438518196474"/>
+</node>
+</node>
+</node>
+</node>
+</map>
